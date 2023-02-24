a03ba1d1011f091c3cff898aa4541424862be6b7
AgustinAraneo <araneoagustin@gmail.com>
2023-02-24 10:38:03 -0300

    Second commit (test git)

--- src/components/desktop/Desktop.jsx
+++ src/components/desktop/Desktop.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Desktop = () => {
   return (
-    <div className='text-blue-500 text-2xl text-center'>Entre Compu</div>
+    <div className='text-blue-500 text-2xl text-center'>Entre Compu
+    <div className='text-purple-500'>
+        Soy pro, estoy probando</div></div>
+
   )
 }
 
