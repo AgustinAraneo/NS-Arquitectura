8b2fdff578833b991902342e692b0c8570ac1e8c
AgustinAraneo <araneoagustin@gmail.com>
2023-03-05 13:50:25 -0300

    Fix de las ultimas actualizaciones v2

--- src/components/desktop/Slick.jsx
+++ src/components/desktop/Slick.jsx
@@ -17,28 +17,28 @@ export default class Slick extends Component {
       <div className="bg-stone-800 py-10 pb-10">
         <Slider {...settings} className="mx-8 cursor-grab">
           <div className="w-full h-[24rem] flex items-center justify-center ">
-            <img src="images/Ruiz/Portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
+            <img src="/images/Ruiz/Portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
           </div>
           <div className="w-full h-[24rem] flex items-center justify-center">
-            <img src="images/Abtao/Portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
+            <img src="/images/Abtao/Portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
           </div>
           <div className="w-full h-[24rem] flex items-center justify-center">
-            <img src="images/Margallo/Portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
+            <img src="/images/Margallo/Portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
           </div>
           <div className="w-full h-[24rem] flex items-center justify-center">
-            <img src="images/Osom/Portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
+            <img src="/images/Osom/Portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
           </div>
           <div className="w-full h-[24rem] flex items-center justify-center">
-            <img src="images/Hilario/Portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
+            <img src="/images/Hilario/Portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
           </div>
           <div className="w-full h-[24rem] flex items-center justify-center">
-            <img src="images/Osom/2.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
+            <img src="/images/Osom/2.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
           </div>
           <div className="w-full h-[24rem] flex items-center justify-center">
-            <img src="images/Osom/3.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
+            <img src="/images/Osom/3.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
           </div>
           <div className="w-full h-[24rem] flex items-center justify-center">
-            <img src="images/Osom/4.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
+            <img src="/images/Osom/4.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
           </div>
         </Slider>
       </div>
