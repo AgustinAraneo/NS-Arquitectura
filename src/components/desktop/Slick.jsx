4e2911d5f0709c5608bda17a543bb87624e2050a
AgustinAraneo <araneoagustin@gmail.com>
2023-03-05 13:56:31 -0300

    Fix de las ultimas actualizaciones v3

--- src/components/desktop/Slick.jsx
+++ src/components/desktop/Slick.jsx
@@ -17,22 +17,22 @@ export default class Slick extends Component {
       <div className="bg-stone-800 py-10 pb-10">
         <Slider {...settings} className="mx-8 cursor-grab">
           <div className="w-full h-[24rem] flex items-center justify-center ">
-            <img src="/images/Ruiz/Portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
+            <img src="https://i.postimg.cc/0N474wqP/portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
           </div>
           <div className="w-full h-[24rem] flex items-center justify-center">
-            <img src="/images/Abtao/Portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
+            <img src="https://i.postimg.cc/GmS2nBcp/portada-Form.png" className="object-cover w-full h-full p-2" alt="Placeholder image" />
           </div>
           <div className="w-full h-[24rem] flex items-center justify-center">
-            <img src="/images/Margallo/Portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
+            <img src="https://i.postimg.cc/yxB6ycTL/portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
           </div>
           <div className="w-full h-[24rem] flex items-center justify-center">
-            <img src="/images/Osom/Portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
+            <img src="https://i.postimg.cc/c4x4K7CH/portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
           </div>
           <div className="w-full h-[24rem] flex items-center justify-center">
-            <img src="/images/Hilario/Portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
+            <img src="https://i.postimg.cc/YCjrRp1B/portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
           </div>
           <div className="w-full h-[24rem] flex items-center justify-center">
-            <img src="/images/Osom/2.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
+            <img src="https://i.postimg.cc/TPDR8C7k/portada.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
           </div>
           <div className="w-full h-[24rem] flex items-center justify-center">
             <img src="/images/Osom/3.jpg" className="object-cover w-full h-full p-2" alt="Placeholder image" />
